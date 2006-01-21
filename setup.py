fba3d2be26663de3e520f3f75b57f9c511b25fdf
jstenar <>
2006-01-21 20:00:17 +0000

    pyreadline: added readme. Changed setup to install to pyreadline

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name="readline",
+setup(name="pyreadline",
       version="1.13-svn",
       description="Python implementation of GNU readline",
       author="Gary Bishop",
       author_email="gb@cs.unc.edu",
-      packages=['readline'],
+      packages=['pyreadline'],
       )
 
