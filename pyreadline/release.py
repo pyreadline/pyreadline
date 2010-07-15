76080fa88590b693c0d96811551c8f2d5e7c12af
Jorgen Stenarson <jorgen.stenarson@bostream.nu>
2010-07-15 19:54:44 +0200

    Fixing release.py for 1.6

--- pyreadline/release.py
+++ pyreadline/release.py
@@ -35,21 +35,20 @@ it is based on the ctypes based UNC readline package by Gary Bishop.
 It is not complete. It has been tested for use with windows 2000 and windows xp.
 
 Features:
- *  NEW: keyboard text selection and copy/paste
+ *  keyboard text selection and copy/paste
  *  Shift-arrowkeys for text selection
- *  Control-c can be used for copy activate with allow_ctrl_c(True) is config file
+ *  Control-c can be used for copy activate with allow_ctrl_c(True) in config file
  *  Double tapping ctrl-c will raise a KeyboardInterrupt, use ctrl_c_tap_time_interval(x)
     where x is your preferred tap time window, default 0.3 s.
  *  paste pastes first line of content on clipboard. 
  *  ipython_paste, pastes tab-separated data as list of lists or numpy array if all data is numeric
  *  paste_mulitline_code  pastes multi line code, removing any empty lines.
- *  Experimental support for ironpython. At this time Ironpython has to be patched for it to work.
  
  
  The latest development version is always available at the IPython subversion
  repository_.
 
-.. _repository: http://ipython.scipy.org/svn/ipython/pyreadline/trunk#egg=pyreadline-dev
+.. _repository:
  """
 
 license = u'BSD'
@@ -69,8 +68,15 @@ platforms = [u'Windows XP/2000/NT',
 keywords = [u'readline',
             u'pyreadline']
 
-classifiers = [u'Development Status :: 4 - Beta',
+classifiers = [u'Development Status :: 5 - Production/Stable',
                u'Environment :: Console',
-               u'Operating System :: Microsoft :: Windows',]
+               u'Operating System :: Microsoft :: Windows',
+               u'License :: OSI Approved :: BSD License',
+               u'Programming Language :: Python :: 2.4',
+               u'Programming Language :: Python :: 2.5',
+               u'Programming Language :: Python :: 2.6',
+               u'Programming Language :: Python :: 2.7',
+
+               ]
                
                
