431135ec7cca9897102a2c9db6bd5c99db9e7700
Jorgen Stenarson <jorgen.stenarson@bostream.nu>
2011-01-11 17:48:31 +0100

    Move logging to its own channel

--- pyreadline/logger.py
+++ pyreadline/logger.py
@@ -13,8 +13,9 @@ host = u"localhost"
 port = logging.handlers.DEFAULT_TCP_LOGGING_PORT
 
 
-root_logger = logging.getLogger(u'')
-root_logger.setLevel(logging.DEBUG)
+pyreadline_logger = logging.getLogger(u'PYREADLINE')
+pyreadline_logger.setLevel(logging.DEBUG)
+pyreadline_logger.propagate = False
 formatter = logging.Formatter('%(message)s')
 file_handler = None
 
@@ -36,29 +37,29 @@ class SocketStream(object):
         pass
 
 socket_handler = None
-root_logger.addHandler(NULLHandler())
+pyreadline_logger.addHandler(NULLHandler())
 
 def start_socket_log():
     global socket_handler
     socket_handler = logging.StreamHandler(SocketStream(host, port))
     socket_handler.setFormatter(formatter)
-    root_logger.addHandler(socket_handler)
+    pyreadline_logger.addHandler(socket_handler)
 
 def stop_socket_log():
     global socket_handler
     if socket_handler:
-        root_logger.removeHandler(socket_handler)
+        pyreadline_logger.removeHandler(socket_handler)
         socket_handler = None
 
 def start_file_log(filename):
     global file_handler
     file_handler = logging.FileHandler(filename, "w")
-    root_logger.addHandler(file_handler)
+    pyreadline_logger.addHandler(file_handler)
 
 def stop_file_log():
     global file_handler
     if file_handler:
-        root_logger.removeHandler(file_handler)
+        pyreadline_logger.removeHandler(file_handler)
         file_handler.close()
         file_handler = None
 
@@ -69,4 +70,4 @@ def stop_logging():
 
 def log(s):
     s = ensure_str(s)
-    root_logger.debug(s)
+    pyreadline_logger.debug(s)
