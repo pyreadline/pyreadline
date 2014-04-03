5216170c269bf26ca33b5260666a81c6c3b225fa
Joshua Hughes <kivhift@gmail.com>
2014-04-03 13:37:18 -0400

    Make it obvious that pyreadline is for Windows only.
    This change makes it so that pyreadline can only be imported on Windows.
    This saves people from wasting time trying to use it on different platforms
    only to get weird results or crashes.

--- pyreadline/__init__.py
+++ pyreadline/__init__.py
@@ -8,6 +8,12 @@
 #*****************************************************************************
 from __future__ import print_function, unicode_literals, absolute_import
 
+from platform import system
+_S = system()
+if 'windows' != _S.lower():
+    raise RuntimeError('pyreadline is for Windows only, not {}.'.format(_S))
+del system, _S
+
 from . import unicode_helper, logger, clipboard, lineeditor, modes, console
 from .rlmain import *
 from . import rlmain
